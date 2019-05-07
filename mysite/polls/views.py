87c1dc49c91df5aaf9e394902aa61e4e355f34dd
NMIRI-kmatsubara <matsubara.kazune@nmiri.city.nagoya.jp>
2019-05-07 17:27:36 +0900

    From F5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Question
@@ -14,11 +14,13 @@ def index(request):
     #return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    try:
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
+    """ try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question' : question})
+    return render(request, 'polls/detail.html', {'question' : question}) """
     #return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
